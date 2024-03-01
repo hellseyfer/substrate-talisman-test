198c37b591cae79bdea951def124133247ee8362
juan montilla <juanmontilla@Desregular.local>
2024-03-01 13:59:23 -0300

    log

--- src/app/features/ws/wsAPI.ts
+++ src/app/features/ws/wsAPI.ts
@@ -3,11 +3,11 @@ import { toDecimal, toPlanckUnit } from '../../utils/unitConversion';
 import { web3FromAddress } from '@polkadot/extension-dapp';
 import log from 'loglevel';
 
-export const WS_URL = import.meta.env.VITE_WS_URL;
+const WS_URL = import.meta.env.VITE_WS_URL;
 const provider = new WsProvider(WS_URL);
 
 let api: ApiPromise;
-export const initializeApi = async () => {
+const initializeApi = async () => {
   if (!api) {
     api = await ApiPromise.create({ provider });
     /**
@@ -18,6 +18,11 @@ export const initializeApi = async () => {
     log.info(`[ws]chainInfo ${chainInfo}`);
   }
   return api;
+};
+
+const disconnect = async () => {
+  await api.disconnect();
+  log.info('\n Disconnected from the wsAPI');
 };
 
 const wsAPI = {
@@ -51,13 +56,24 @@ const wsAPI = {
       tx.signAndSend(fromAddress, { signer: injector.signer }, ({ status }) => {
         if (status.isInBlock) {
           log.info(
-            `[ws] Completed at block hash#'${status.asInBlock.toString()}`
+            `\n\n######################################## Transaction pending ###########################################`
+          );
+          log.info(
+            `\n Transaction included at blockHash : ${status.asInBlock}`
+          );
+          log.info(
+            `\n Check block status for pending transaction on the Subscan explorer : https://westend.subscan.io/block/${status.asInBlock}`
           );
           resolve(status.asInBlock.toString()); // Resolve with block hash
         } else if (status.isFinalized) {
-          // If you want to handle finalized status separately
           log.info(
-            `[ws] Transaction finalized at block hash#'${status.asFinalized.toString()}`
+            `\n\n######################################## Transaction successful and finalized ##########################################`
+          );
+          log.info(
+            `\n Transaction finalized at blockHash : ${status.asFinalized}`
+          );
+          log.info(
+            `\n Check block status for finalized transaction on the Subscan explorer : https://westend.subscan.io/block/${status.asFinalized}`
           );
         } else if (status.isDropped || status.isInvalid) {
           reject(new Error(`Transaction failed with status: ${status.type}`));
@@ -74,4 +90,4 @@ const wsAPI = {
   },
 };
 
-export { wsAPI };
+export { wsAPI, disconnect, initializeApi };
